95e1d431c79aee67fff3adaa60310d158fa9d0f1
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-07-21 20:05:38 +0530

    bishop

--- client/src/types/chessTypes.ts
+++ client/src/types/chessTypes.ts
@@ -9,13 +9,32 @@ export type ChessPieceName =
   | "queen"
   | "king"
   | "";
+
 export type ValidMoveType = "valid";
 export type CapturingMoveType = "capturing";
 export type ProtectingMoveType = "protecting";
+
 export type ValidChessMove = {
   [key: string]: ValidMoveType | CapturingMoveType;
 };
 export type ProtectingChessMove = {
   [key: string]: ProtectingMoveType;
 };
+
 export type ChessBoardType = (ChessPiece | number | "dot")[][];
+
+export type KingParametersType = {
+  blackKingInCheck: boolean;
+  whiteKingInCheck: boolean;
+  pieceCheckingWhiteKing: ChessPiece | null;
+  pieceCheckingBlackKing: ChessPiece | null;
+  whiteKingPos: [number, number];
+  blackKingPos: [number, number];
+  cellsUnderAttackByWhite: ValidChessMove | ProtectingChessMove;
+  cellsUnderAttackByBlack: ValidChessMove | ProtectingChessMove;
+};
+
+export type GenericKingParametersType = {
+  pieceCheckingKing: ChessPiece | null;
+  kingPos: [number, number];
+};
