dd76185f450347fef754d0af6eebbd58526926f3
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-07-26 13:47:20 +0530

    finish refactoring server

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,6 @@
 # /media/pragyan/Local Disk/Python/MultiplayerGamesFastAPITS/server/env/bin/python3
 
 import os
-from sockets.mainSockets import get_bot_message, get_room
 from helpers.colors import COLORS
 from config.Config import Colors, GameNames, SocketEvents
 from helpers.printHelper import new_line_print
@@ -15,6 +14,7 @@ from routes import userRoutes
 from fastapi.middleware.cors import CORSMiddleware
 import random
 
+from sockets.mainSockets import SocketHandler
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, BASE_DIR)
@@ -41,31 +41,7 @@ app = socketio.ASGIApp(
     socketio_server=socket, other_asgi_app=fast_app, socketio_path="/socket.io/"
 )
 
-"""  
-{
-    game_name: {
-        room_id: [
-            user_1_username, user_2_username,....
-        ]
-    }
-}
-"""
-ROOMS: "dict[str, dict[str, list[str]]]" = {
-    GameNames.CHESS: {},
-    GameNames.CHECKERS: {},
-    GameNames.SKETCHIO: {},
-}
-
-"""  
-{
-    socket_id: {
-        username: username,
-        room_id: room_id,
-        game_name: game_name
-    }
-}
-"""
-socket_id_to_username: "dict[str, dict[str, str]]" = {}
+socket_handler = SocketHandler(socket)
 
 
 @socket.event
@@ -75,189 +51,42 @@ def connect(socket_id, data):
 
 @socket.event
 async def disconnect(socket_id):
-    socket_info = socket_id_to_username[socket_id]
-    username = socket_info["username"]
-    game_name = socket_info["game_name"]
-    room_id = socket_info["room_id"]
-
-    message = get_bot_message(username, False)
-
-    # delete the user from the room
-    ROOMS[game_name][room_id] = list(
-        filter(
-            lambda x: x != username,
-            ROOMS[game_name][room_id],
-        )
-    )
-
-    # free up some memory
-    del socket_id_to_username[socket_id]
-
-    await socket.emit(
-        SocketEvents.RECEIVE_CHAT_MESSAGE,
-        message,
-        to=room_id,
-        skip_sid=socket_id,
-    )
+    await socket_handler.disconnect(socket_id)
 
 
 @socket.event
 async def userPlayedAMove(socket_id, move: "dict[str, list[int]]"):
-    room_to_send_to = get_room(socket, socket_id)
-
-    await socket.emit(
-        SocketEvents.OPPONENT_PLAYED_A_MOVE,
-        move,
-        skip_sid=socket_id,
-        to=room_to_send_to,
-    )
+    await socket_handler.userPlayedAMove(socket_id, move)
 
 
 @socket.event
 async def joinRoom(socket_id, data: "dict[str, str]"):
-    # extract the game name from the room_id
-    # room_id = <gameName>_<actualRoomId>
-
-    game_name, room_id = data["roomId"].split("_")
-    username = data["username"]
-
-    socket_id_to_username[socket_id] = {
-        "username": username,
-        "game_name": game_name,
-        "room_id": room_id,
-    }
-
-    socket.enter_room(socket_id, room_id)
-
-    random_chat_color = COLORS[random.randint(0, len(COLORS) - 1)]
-
-    # add the new socket to the room
-    if not ROOMS[game_name].get(room_id):
-        # newly created room, only one player's in it
-        ROOMS[game_name][room_id] = [username]
-
-    else:
-        # someone's already in the room so append the next player
-        ROOMS[game_name][room_id].append(username)
-
-    if game_name == GameNames.CHESS:
-        if len(ROOMS[game_name][room_id]) == 1:
-            # first player in a chess room, assign them the color white
-            color = Colors.WHITE
-
-        else:
-            # one player is already in the room, so assign the color black
-            color = Colors.BLACK
-
-            # send a list of the two users who are playeing the game
-            await socket.emit(
-                SocketEvents.CHESS_PLAYER_2_JOINED,
-                {"users": ROOMS[game_name][room_id]},
-                to=room_id,
-            )
-
-        await socket.emit(
-            SocketEvents.CHESS_COLOR_SELECTED,
-            {"color": color, "chatColor": random_chat_color},
-            to=socket_id,
-        )
-
-    elif game_name == GameNames.CHECKERS:
-        if len(ROOMS[game_name][room_id]) == 1:
-            # first player in a chess room, assign them the color white
-            color = Colors.WHITE
-
-        else:
-            # one player is already in the room, so assign the color black
-            color = Colors.RED
-
-            # send a list of the two users who are playeing the game
-            await socket.emit(
-                SocketEvents.CHECKERS_PLAYER_2_JOINED,
-                {"users": ROOMS[game_name][room_id]},
-                to=room_id,
-            )
-
-        await socket.emit(
-            SocketEvents.CHECKERS_COLOR_SELECTED,
-            {"color": color, "chatColor": random_chat_color},
-            to=socket_id,
-        )
-
-    # emit a chat message from a bot
-    await socket.emit(
-        SocketEvents.RECEIVE_CHAT_MESSAGE,
-        get_bot_message(username, connected=True),
-        to=room_id,
-        skip_sid=socket_id,
-    )
+    await socket_handler.joinRoom(socket_id, data)
 
 
 @socket.event
 async def sentChatMessage(socket_id, data):
-    """
-    data = {
-        username: string;
-        color: string;
-        message: string;
-    }
-    """
-    room_to_send_to = get_room(socket, socket_id)
-
-    await socket.emit(
-        SocketEvents.RECEIVE_CHAT_MESSAGE,
-        data,
-        room=room_to_send_to,
-        skip_sid=socket_id,
-    )
+    await socket_handler.sentChatMessage(socket_id, data)
 
 
 @socket.event
 async def checkersGameOver(socket_id, gameOverData):
-    room_to_send_to = get_room(socket, socket_id)
-
-    await socket.emit(
-        SocketEvents.CHECKERS_GAME_OVER,
-        gameOverData,
-        room=room_to_send_to,
-        skip_sid=socket_id,
-    )
+    await socket_handler.checkersGameOver(socket_id, gameOverData)
 
 
 @socket.event
 async def beganPath(socket_id, data):
-    room_to_send_to = get_room(socket, socket_id)
-
-    await socket.emit(
-        SocketEvents.BEGAN_PATH,
-        data,
-        room=room_to_send_to,
-        skip_sid=socket_id,
-    )
+    socket_handler.beganPath(socket_id, data)
 
 
 @socket.event
 async def strokedPath(socket_id, data):
-    room_to_send_to = get_room(socket, socket_id)
-
-    await socket.emit(
-        SocketEvents.STROKED_PATH,
-        data,
-        room=room_to_send_to,
-        skip_sid=socket_id,
-    )
+    socket_handler.strokedPath(socket_id, data)
 
 
 @socket.event
 async def startedFilling(socket_id, data):
-    room_to_send_to = get_room(socket, socket_id)
-
-    await socket.emit(
-        SocketEvents.STARTED_FILLING,
-        data,
-        room=room_to_send_to,
-        skip_sid=socket_id,
-    )
+    socket_handler.startedFilling(socket_id, data)
 
 
 @fast_app.get("/")
--- server/src/sockets/mainSockets.py
+++ server/src/sockets/mainSockets.py
@@ -1,10 +1,211 @@
 from socketio.asyncio_server import AsyncServer
-from config.Config import Colors
+from config.Config import Colors, GameNames, SocketEvents
+
+from helpers.colors import COLORS
+import random
+
+
+"""  
+ROOMS = {
+    game_name: {
+        room_id: [
+            user_1_username, user_2_username,....
+        ]
+    }
+}
+"""
+
+"""  
+socket_id_to_username = {
+    socket_id: {
+        username: username,
+        room_id: room_id,
+        game_name: game_name
+    }
+}
+"""
 
 
 class SocketHandler:
     def __init__(self, socket: AsyncServer) -> None:
         self.socket = socket
+        self.socket_id_to_username: "dict[str, dict[str, str]]" = {}
+        self.ROOMS: "dict[str, dict[str, list[str]]]" = {
+            GameNames.CHESS: {},
+            GameNames.CHECKERS: {},
+            GameNames.SKETCHIO: {},
+        }
+
+    async def joinRoom(self, socket_id, data: "dict[str, str]"):
+        # extract the game name from the room_id
+        # room_id = <gameName>_<actualRoomId>
+
+        game_name, room_id = data["roomId"].split("_")
+        username = data["username"]
+
+        self.socket_id_to_username[socket_id] = {
+            "username": username,
+            "game_name": game_name,
+            "room_id": room_id,
+        }
+
+        self.socket.enter_room(socket_id, room_id)
+
+        random_chat_color = COLORS[random.randint(0, len(COLORS) - 1)]
+
+        # add the new socket to the room
+        if not self.ROOMS[game_name].get(room_id):
+            # newly created room, only one player's in it
+            self.ROOMS[game_name][room_id] = [username]
+
+        else:
+            # someone's already in the room so append the next player
+            self.ROOMS[game_name][room_id].append(username)
+
+        if game_name == GameNames.CHESS:
+            if len(self.ROOMS[game_name][room_id]) == 1:
+                # first player in a chess room, assign them the color white
+                color = Colors.WHITE
+
+            else:
+                # one player is already in the room, so assign the color black
+                color = Colors.BLACK
+
+                # send a list of the two users who are playeing the game
+                await self.socket.emit(
+                    SocketEvents.CHESS_PLAYER_2_JOINED,
+                    {"users": self.ROOMS[game_name][room_id]},
+                    to=room_id,
+                )
+
+            await self.socket.emit(
+                SocketEvents.CHESS_COLOR_SELECTED,
+                {"color": color, "chatColor": random_chat_color},
+                to=socket_id,
+            )
+
+        elif game_name == GameNames.CHECKERS:
+            if len(self.ROOMS[game_name][room_id]) == 1:
+                # first player in a chess room, assign them the color white
+                color = Colors.WHITE
+
+            else:
+                # one player is already in the room, so assign the color black
+                color = Colors.RED
+
+                # send a list of the two users who are playeing the game
+                await self.socket.emit(
+                    SocketEvents.CHECKERS_PLAYER_2_JOINED,
+                    {"users": self.ROOMS[game_name][room_id]},
+                    to=room_id,
+                )
+
+            await self.socket.emit(
+                SocketEvents.CHECKERS_COLOR_SELECTED,
+                {"color": color, "chatColor": random_chat_color},
+                to=socket_id,
+            )
+
+        # emit a chat message from a bot
+        await self.socket.emit(
+            SocketEvents.RECEIVE_CHAT_MESSAGE,
+            self.get_bot_message(username, connected=True),
+            to=room_id,
+            skip_sid=socket_id,
+        )
+
+    async def userPlayedAMove(self, socket_id, move: "dict[str, list[int]]"):
+        room_to_send_to = self.get_room(socket_id)
+
+        await self.socket.emit(
+            SocketEvents.OPPONENT_PLAYED_A_MOVE,
+            move,
+            skip_sid=socket_id,
+            to=room_to_send_to,
+        )
+
+    async def sentChatMessage(self, socket_id, data):
+        """
+        data = {
+            username: string;
+            color: string;
+            message: string;
+        }
+        """
+        room_to_send_to = self.get_room(socket_id)
+
+        await self.socket.emit(
+            SocketEvents.RECEIVE_CHAT_MESSAGE,
+            data,
+            room=room_to_send_to,
+            skip_sid=socket_id,
+        )
+
+    async def checkersGameOver(self, socket_id, gameOverData):
+        room_to_send_to = self.get_room(socket_id)
+
+        await self.socket.emit(
+            SocketEvents.CHECKERS_GAME_OVER,
+            gameOverData,
+            room=room_to_send_to,
+            skip_sid=socket_id,
+        )
+
+    async def disconnect(self, socket_id):
+        socket_info = self.socket_id_to_username[socket_id]
+        username = socket_info["username"]
+        game_name = socket_info["game_name"]
+        room_id = socket_info["room_id"]
+
+        message = self.get_bot_message(username, False)
+
+        # delete the user from the room
+        self.ROOMS[game_name][room_id] = list(
+            filter(
+                lambda x: x != username,
+                self.ROOMS[game_name][room_id],
+            )
+        )
+
+        # free up some memory
+        del self.socket_id_to_username[socket_id]
+
+        await self.socket.emit(
+            SocketEvents.RECEIVE_CHAT_MESSAGE,
+            message,
+            to=room_id,
+            skip_sid=socket_id,
+        )
+
+    async def beganPath(self, socket_id, data):
+        room_to_send_to = self.get_room(socket_id)
+
+        await self.socket.emit(
+            SocketEvents.BEGAN_PATH,
+            data,
+            room=room_to_send_to,
+            skip_sid=socket_id,
+        )
+
+    async def strokedPath(self, socket_id, data):
+        room_to_send_to = self.get_room(socket_id)
+
+        await self.socket.emit(
+            SocketEvents.STROKED_PATH,
+            data,
+            room=room_to_send_to,
+            skip_sid=socket_id,
+        )
+
+    async def startedFilling(self, socket_id, data):
+        room_to_send_to = self.get_room(socket_id)
+
+        await self.socket.emit(
+            SocketEvents.STARTED_FILLING,
+            data,
+            room=room_to_send_to,
+            skip_sid=socket_id,
+        )
 
     def get_bot_message(self, username: str, connected: bool) -> "dict[str, str]":
         to_add = "joined" if connected else "left"
