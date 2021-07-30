93409e4c0445f5e89722b7d7681865616ef80c83
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-07-30 21:56:00 +0530

    display statistics

--- client/src/components/user/CircularGraph.tsx
+++ client/src/components/user/CircularGraph.tsx
@@ -3,21 +3,26 @@ import React from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core";
 
 interface CircularGraphProps {
   percentage: number;
+  color: string;
 }
 
-const CircularGraph: React.FC<CircularGraphProps> = ({ percentage }) => {
+const CircularGraph: React.FC<CircularGraphProps> = ({ percentage, color }) => {
+  const classes = makeStyles(t => ({
+    circle: {
+      "& svg circle": {
+        stroke: percentage !== 0 ? color : "white"
+      }
+    }
+  }))();
+
   return (
     <div>
-      <Box position="relative" display="inline-flex">
-        <CircularProgress
-          size="3rem"
-          variant="determinate"
-          value={percentage}
-          color="secondary"
-        />
+      <Box position="relative" display="inline-flex" className={classes.circle}>
+        <CircularProgress size="3rem" variant="determinate" value={percentage || 100} />
         <Box
           top={0}
           left={0}
--- client/src/views/Statistics.tsx
+++ client/src/views/Statistics.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
@@ -10,15 +10,17 @@ import Typography from "@material-ui/core/Typography";
 
 import CircularGraph from "../components/user/CircularGraph";
 import { useTypedSelector } from "../hooks/useTypedSelector";
-import { useEffect } from "react";
 import { axiosInstance } from "../config/axiosConfig";
+import { ListSubheader } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: "100%",
       maxWidth: "36ch",
-      backgroundColor: "rgb(40, 40, 40)"
+      backgroundColor: "#0B1622",
+      borderRadius: "0.25rem",
+      boxShadow: "0 0 5px gray"
     },
     inline: {
       display: "inline"
@@ -26,10 +28,36 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-const getUserData = async (username: string) => {
+type KeyNames = "games_drawn" | "games_lost" | "games_started" | "games_won";
+
+type ApiResponse = {
+  [k in KeyNames]: number;
+};
+
+interface StatsState {
+  chess: ApiResponse;
+  checkers: ApiResponse;
+}
+
+const getUserData = async (username: string): Promise<StatsState> => {
   const { data } = await axiosInstance.get(`/games/${username}`);
 
-  console.log(data);
+  const object: StatsState = {
+    chess: {
+      games_won: data.chess_info.games_won,
+      games_lost: data.chess_info.games_lost,
+      games_drawn: data.chess_info.games_drawn,
+      games_started: data.chess_info.games_started
+    },
+    checkers: {
+      games_won: data.checkers_info.games_won,
+      games_lost: data.checkers_info.games_lost,
+      games_drawn: data.checkers_info.games_drawn,
+      games_started: data.checkers_info.games_started
+    }
+  };
+
+  return object;
 };
 
 const Statistics: React.FC = () => {
@@ -37,40 +65,86 @@ const Statistics: React.FC = () => {
 
   const { user } = useTypedSelector(state => state);
 
+  const [statistics, setStatistics] = useState<StatsState>({} as StatsState);
+
   useEffect(() => {
-    getUserData(user.username);
+    getUserData(user.username).then(stats => setStatistics(stats));
   }, []);
 
+  const capitalize = (word: string) => {
+    return word
+      .split("_")
+      .map(c => c[0].toUpperCase() + c.slice(1))
+      .join(" ");
+  };
+
+  const getColor: { [k in KeyNames]: string } = {
+    games_won: "#27ae60",
+    games_lost: "#e74c3c",
+    games_drawn: "#7f8c8d",
+    games_started: "#16a085"
+  };
+
   return (
-    <List className={classes.root}>
-      {new Array(3).fill(0).map(e => (
-        <>
-          {" "}
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <CircularGraph percentage={100} />
-            </ListItemAvatar>
-            <ListItemText
-              style={{ marginLeft: "1rem" }}
-              primary="Games Won"
-              secondary={
-                <>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="error"
-                  >
-                    24 out of 400
-                  </Typography>
-                </>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
-        </>
-      ))}
-    </List>
+    <div
+      style={{
+        minHeight: "92vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around"
+      }}
+    >
+      {Object.entries(statistics).map(
+        ([gameName, gameStatsObject]: [string, ApiResponse]) => (
+          <List
+            className={classes.root}
+            key={gameName}
+            subheader={
+              <ListSubheader
+                component="h1"
+                style={{ color: "inherit", fontSize: "1.2rem", fontWeight: "bold" }}
+              >
+                {capitalize(gameName)}
+              </ListSubheader>
+            }
+          >
+            {Object.entries(gameStatsObject).map(([key, value]: [string, number]) => (
+              <>
+                <ListItem alignItems="flex-start" key={key}>
+                  <ListItemAvatar>
+                    <CircularGraph
+                      percentage={
+                        gameStatsObject.games_started === 0
+                          ? 0
+                          : (value / gameStatsObject.games_started) * 100
+                      }
+                      color={getColor[key as KeyNames]}
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    style={{ marginLeft: "1rem" }}
+                    primary={capitalize(key)}
+                    secondary={
+                      <>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          style={{ color: getColor[key as KeyNames] }}
+                        >
+                          {value} out of {gameStatsObject.games_started}
+                        </Typography>
+                      </>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </>
+            ))}
+          </List>
+        )
+      )}
+    </div>
   );
 };
 
